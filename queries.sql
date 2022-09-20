b3e266627c4d68f91c06afc512570e2a5aa3e4ab
Josh <joshtesh91@mail.com>
2022-09-20 14:05:52 +0800

    fix the comment given by my code reviewer

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * from animals WHERE name LIKE %mon;
 SELECT * FROM animals WHERE date_of_birth BETWEEN CAST('2016-01-01' AS DATE) AND CAST('2019-12-30' AS DATE);
-SELECT * FROM animals WHERE neutered = false AND escape_attempts < 3;
+SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
