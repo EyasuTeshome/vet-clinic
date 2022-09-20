cff079c02ad8ff8d466a9b7f72ff01182bc5c846
Josh <joshtesh91@mail.com>
2022-09-20 23:17:55 +0800

    update queries

--- queries.sql
+++ queries.sql
@@ -32,3 +32,17 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO weight_kg;
 
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg <0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals GROUP BY species WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01';
