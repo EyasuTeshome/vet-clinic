a634c9ed230ed4445f35142f70b7266aac84438d
Collins <mussocollins@gmail.com>
2022-09-27 18:30:24 +0300

    Add two tables invoice and invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -17,3 +17,26 @@ CREATE TABLE medical_histories (
   PRIMARY KEY(id) ,
   CONSTRAINT fk_patients FOREIGN KEY (patient_id) REFERENCES Patients (id) ON DELETE CASCADE
 )
+
+-- Invoices Table
+
+CREATE TABLE invoices(
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  total_amount MONEY NOT NULL,
+  generated_at DATE NOT NULL,
+  payed_at DATE NOT NULL,
+  medical_history_id INT NOT NULL,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(medical_history_id) ON DELETE CASCADE
+)
+
+-- Invoice Items Table
+CREATE TABLE invoice_items(
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  FOREIGN KEY (invoice_id) REFERENCES invoices(invoice_id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(treatment_id)
+)
